bcbc8789de74e5a2669bfd21e38ba3988ce32389
Dhananjay Hawal <39691422+dhananjay93@users.noreply.github.com>
2022-05-12 23:04:51 +0530

    Create leetcode608.sql
    Tree node

---
+++ sql/leetcode608.sql
@@ -0,0 +1,12 @@
+---Learnings case statements, Subquery
+#https://leetcode.com/problems/tree-node/
+# Write your MySQL query statement below
+SELECT
+ id,
+ CASE 
+    WHEN p_id IS NULL THEN 'Root'
+    WHEN id IN (SELECT p_id FROM Tree) THEN 'Inner'
+    ELSE 'Leaf'
+ END AS type
+FROM Tree
+ORDER BY id
